fc7d9f4a9e61b233f1eb845b1be52b4199e6e414
n0b136 <50467142+N0b136@users.noreply.github.com>
2019-06-29 21:08:49 -0500

    Completed DOM step 1 and 2, refactored

---
+++ DOM/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+//number 1
+let btn = document.createElement('button');
+let btnTxt = document.createTextNode('Hello');
+btn.appendChild(btnTxt);
+document.getElementById('1').appendChild(btn);
+
+btn.addEventListener("click", function() {
+    alert("Have a Wonderful day.");
+})
+
+//number 2
+let btn2 = document.createElement('button');
+let btn2Text = document.createTextNode('Alert');
+btn2.appendChild(btn2Text);
+document.getElementById('2').appendChild(btn2);
+
+let textBox = document.createElement('input');
+textBox.setAttribute('type', 'text');
+textBox.id = 'textBox';
+document.getElementById('2').appendChild(textBox);
+
+btn2.addEventListener('click', function() {
+    let tbInput = textBox.value;
+    alert(tbInput);
+});
+
+// number 3
+
+
+
+
+
+
+
+}); //END
